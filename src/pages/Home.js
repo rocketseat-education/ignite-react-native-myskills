100f3dd68a2fde0020c6708dc45aea3d738ef74e
Dalton Menezes <daltonmenezes@outlook.com>
2021-05-27 22:41:02 -0300

    A imutabilidade no React utilizando o useState

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import {
     View,
     Text,
@@ -9,6 +9,14 @@ import {
 } from 'react-native';
 
 export function Home(){
+  const [newSkill, setNewSkill] = useState('');
+  const [mySkills, setMySkills] = useState([]);
+
+  function handleAddNewSkill(){
+    setMySkills(oldState => [...oldState, newSkill]);
+  }
+
+
   return (
       <View style={styles.container}>
         <Text style={styles.title}>
@@ -19,18 +27,30 @@ export function Home(){
             style={styles.input}
             placeholder="New skill"
             placeholderTextColor="#555"
+            onChangeText={setNewSkill}
         />
 
         <TouchableOpacity
             style={styles.button}
             activeOpacity={.7}
+            onPress={handleAddNewSkill}
         >
             <Text style={styles.buttonText}>Add</Text>
         </TouchableOpacity>
 
-        <Text style={[styles.title, { marginTop: 50 }]}>
+        <Text style={[styles.title, { marginVertical: 50}]}>
             My Skills    
         </Text>
+
+        {
+            mySkills.map(skill => (
+                <TouchableOpacity key={skill} style={styles.buttonSkill}>
+                    <Text style={styles.textSkill}>
+                        {skill}
+                    </Text>
+                </TouchableOpacity>
+            ))
+        }
 
 
       </View>
@@ -68,6 +88,18 @@ const styles = StyleSheet.create({
     buttonText: {
         color: '#FFF',
         fontSize: 17,
+        fontWeight: 'bold',
+    },
+    buttonSkill: {
+        backgroundColor: '#1F1E25',
+        padding: 15,
+        borderRadius: 50,
+        alignItems: 'center',
+        marginVertical: 10
+    },
+    textSkill: {
+        color: '#FFF',
+        fontSize: 22,
         fontWeight: 'bold',
     }
 })
