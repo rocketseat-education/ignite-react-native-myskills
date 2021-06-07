cbee1f83058909b855f4823f314560c86b64d787
Dalton Menezes <daltonmenezes@outlook.com>
2021-06-07 14:37:01 -0300

    Scroll View x FlatList

--- src/components/SkillCard.js
+++ src/components/SkillCard.js
@@ -7,7 +7,7 @@ import {
 
 export function SkillCard({ skill }){
     return (
-        <TouchableOpacity key={skill} style={styles.buttonSkill}>
+        <TouchableOpacity style={styles.buttonSkill}>
             <Text style={styles.textSkill}>
                 {skill}
             </Text>
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -5,6 +5,7 @@ import {
     StyleSheet,
     TextInput,
     Platform,
+    FlatList
 } from 'react-native';
 
 import { Button } from '../components/Button';
@@ -38,12 +39,13 @@ export function Home(){
             My Skills    
         </Text>
 
-        {
-            mySkills.map(skill => (
-                <SkillCard skill={skill}/>
-            ))
-        }
-
+        <FlatList
+            data={mySkills}
+            keyExtractor={item => item}
+            renderItem={({ item }) => (
+                <SkillCard skill={item}/>
+            )}
+        />
 
       </View>
   )
