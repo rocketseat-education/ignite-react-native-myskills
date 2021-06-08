dca31ff91d653b11b3717a768452c7d30c82d6c0
Dalton Menezes <daltonmenezes@outlook.com>
2021-06-08 15:54:06 -0300

    Finalizando o nosso App

--- src/components/SkillCard.tsx
+++ src/components/SkillCard.tsx
@@ -1,13 +1,21 @@
 import React from 'react';
 import {
     TouchableOpacity,
+    TouchableOpacityProps,
     Text,
     StyleSheet
 } from 'react-native';
 
-export function SkillCard({ skill }){
+interface SkillCardProps extends TouchableOpacityProps {
+    skill: string;
+}
+
+export function SkillCard({ skill, ...rest } : SkillCardProps){
     return (
-        <TouchableOpacity style={styles.buttonSkill}>
+        <TouchableOpacity
+            style={styles.buttonSkill}
+            {...rest}
+        >
             <Text style={styles.textSkill}>
                 {skill}
             </Text>
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -31,6 +31,12 @@ export function Home(){
     setMySkills(oldState => [...oldState, data]);
   }
 
+  function handleRemoveSkill(id: string){
+      setMySkills(oldState => oldState.filter(
+          skill => skill.id !== id
+      ));
+  }
+
 
   useEffect(() => {
       const currentHour = new Date().getHours();
@@ -78,7 +84,10 @@ export function Home(){
             data={mySkills}
             keyExtractor={item => item.id}
             renderItem={({ item }) => (
-                <SkillCard skill={item.name} />
+                <SkillCard
+                    skill={item.name}
+                    onPress={() => handleRemoveSkill(item.id)}
+                />
             )}
         />
 
@@ -90,7 +99,6 @@ const styles = StyleSheet.create({
     container: {
         flex: 1,
         backgroundColor: '#121015',
-        paddingHorizontal: 20,
         paddingVertical: 70,
         paddingHorizontal: 30
     },
